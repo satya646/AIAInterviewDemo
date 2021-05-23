43822fc10cffd168b46a824556b1211067173140
satya646 <satya.appsdeveloper@gmail.com>
2021-05-23 15:42:20 +0530

    First screen design

---
+++ AIAInterviewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIAInterviewProject
+//
+//  Created by Satya on 22/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
